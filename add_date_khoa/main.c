9e42d2312391e0c5e0c2a17664818002cb69fde5
khoa-momo <khoalnv83@gmail.com>
2022-09-14 16:13:24 +0700

    add date time

---
+++ add_date_khoa/main.c
@@ -0,0 +1,18 @@
+#include"FAT_system.h"
+#include"FAT_system.c"
+
+int main(){
+	fp = fopen("floppy.img","rb");
+	
+//	int i;
+//	Shift_Offset(0x300);
+//	for(i=0;i<512;i++){
+//		printf("%x-",fgetc(fp));
+//	}
+
+	//readData(); 
+ 	readDataNode();
+	fclose(fp);
+	
+	return 0;
+}
